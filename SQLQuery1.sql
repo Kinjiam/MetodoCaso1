9a7c893f2ada4d611e64e3f1ef803a3a92944b6b
Kinjiam <maya.jimena@yahoo.com>
2025-10-20 23:01:28 -0600

    Delete SQLQuery1.sql
    Archivo erroneo

--- SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    OBJECT_NAME(parent_id) AS Tabla,
-    name AS NombreTrigger,
-    create_date AS FechaCreacion,
-    is_disabled AS EstaDeshabilitado
-FROM sys.triggers
-ORDER BY Tabla, NombreTrigger;
-
